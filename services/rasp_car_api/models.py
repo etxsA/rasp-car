07ba1e1a4d4a6f9e8b9a3cd00e49e20d7233563a
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-04 09:36:31 -0600

    Added ORM models

--- services/rasp_car_api/models.py
+++ services/rasp_car_api/models.py
@@ -1,11 +1,38 @@
 # Definition of ORM models for SQLAlchemy
 # main/models.py
-from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy import Column, Integer, Double, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
-class Sensor1(Base):
-    __tablename__ = "s1"
+Base = declarative_base()
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50))
-
+class Photoresistor(Base):
+    __tablename__ = "photoresistor"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    analog_voltage = Column(Integer, nullable=False)
+    voltage = Column(Double, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+class Accelerometer(Base):
+    __tablename__ = "accelerometer"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    x_axis = Column(Double, nullable=False)
+    y_axis = Column(Double, nullable=False)
+    z_axis = Column(Double, nullable=False)
+    free_fall = Column(Boolean, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+class Distance(Base):
+    __tablename__ = "distance"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    distance = Column(Double, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+class Pressure(Base):
+    __tablename__ = "pressure"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    temperature = Column(Double, nullable=False)
+    pressure = Column(Double, nullable=False)
+    altitude = Column(Double, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
