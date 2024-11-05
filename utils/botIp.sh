9445989cfebb22ec6bb02066d26d95547d059f99
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-04 23:12:37 -0600

    Correction of sh

--- utils/botIp.sh
+++ utils/botIp.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 #Edit your creds
-botToken ="token"
-chatId ="chatId"
+botToken="token"
+chatId="chatId"
 
 # Get the Raspberry Pi's local IP address
-ipAddr = $(hostname -I | awk '{print $1}')
+ipAddr=$(hostname -I | awk '{print $1}')
 
 # Compose the message
-message = "Your Raspberry Pi IP address is: $ipAddr"
+message="Your Raspberry Pi IP address is: $ipAddr"
 
 # Send the message via Telegram API using curl
 curl -s -X POST "https://api.telegram.org/bot$botToken/sendMessage" \
      -d chat_id="$chatId" \
      -d text="$message"
 
-user = ${whoami}
 
-echo "Sent IP address $ipAddr to Telegram from user $" >> /home/$user/ip_address_log.txt
+echo "Sent IP address $ipAddr to Telegram from user $USER" >> /home/$USER/ip_address_log.txt
