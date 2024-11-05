162ba7848990bc5b84b17e269a8f4ecc2bc4525a
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-04 23:48:17 -0600

    Correction of service creation

--- utils/makeBotService.sh
+++ utils/makeBotService.sh
@@ -3,6 +3,8 @@
 sudo cp ./botIp.sh /usr/local/bin/botIp.sh
 sudo chmod +x /usr/local/bin/botIp.sh
 sudo cp ./telegramBot.service /etc/systemd/system/telegramBot.service
+sudo chown root:root /etc/systemd/system/telegramBot.service
+sudo chmod 644 /etc/systemd/system/telegramBot.service
 sudo systemctl daemon-reload
 sudo systemctl enable telegramBot.service
 sudo systemctl status telegramBot.service
