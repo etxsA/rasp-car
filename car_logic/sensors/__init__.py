73fd77824cb17ca6709358d23b5423bdf77f1197
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-06 18:02:49 -0600

    Start implementation of ADXL345

--- car_logic/sensors/__init__.py
+++ car_logic/sensors/__init__.py
@@ -1,3 +1,4 @@
 from .sense import SensorController
 from .ultrasonic import Ultrasonic
 from .bmp280 import BMP280
+from .adxl345 import ADXL345
--- car_logic/test.py
+++ car_logic/test.py
@@ -1,10 +1,14 @@
 from .sensors import Ultrasonic
 from .sensors import BMP280
+from .sensors import ADXL345
+
 ultratest = Ultrasonic()
 bmptest = BMP280()
+acctest = ADXL345()
 
 print(f"Distance in cm: {ultratest.measureDistance()}")
 print(bmptest.readData())
+print(acctest.readData())
 
 
 
