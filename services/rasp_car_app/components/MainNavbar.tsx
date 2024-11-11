71d75681f36af778b3fbdefdf37ab861feada1f1
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-11 00:37:53 -0600

    Landing Page Bg Ready

---
+++ services/rasp_car_app/components/MainNavbar.tsx
@@ -0,0 +1,12 @@
+// component/MainNavbar.tsx
+export default function MainNavbar() {
+
+    return (
+        <nav>
+            <div className="flex flex-row items-center p-2 gap-2 \
+            bg-[--navColor] border-[0.5px] boder-[#DCDCDC] shadow-[4px_4px_4px_rgba(0,0,0,0.3)] rounded-[8px] p-4">
+                Test For Navbar Logo Title
+            </div>
+        </nav>
+    );
+}
--- services/rasp_car_app/tailwind.config.ts
+++ services/rasp_car_app/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'source-code-pro': ['"Source Code Pro"', 'monospace'],
+      },
     },
   },
   plugins: [],
