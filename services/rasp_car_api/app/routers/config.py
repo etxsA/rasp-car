d0d58ff490b76e11e516e1ed2ef38ca2217a48c4
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-11 20:19:29 -0600

    feat: Add and implement mqtt with fast api, delte unused from David's commit

--- services/rasp_car_api/app/routers/config.py
+++ services/rasp_car_api/app/routers/config.py
@@ -1,9 +1,11 @@
 # Implementation of route to send API
 # main/routers/pressure.py
 import os 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter
+from dotenv import load_dotenv
 from app import schemas
 
+load_dotenv()
 router = APIRouter()
 
 # Edit Dict to modify configs
@@ -12,7 +14,7 @@ configuration = {
     # sensor : {},
     "mqtt": {
         "broker": os.getenv("MQTT_BROKER", "broker.hivemq.com"),
-        "port" :  os.getenv("MQTT_PORT", 1883),
+        "port" :  int(os.getenv("MQTT_PORT", 1883)),
         "topic": os.getenv("MQTT_TOPIC", "defTopic"),
     },
     "sql": os.getenv("SQL_URL", "mysql+pymysql://root:toor123@10.48.229.221:3306/Sensors"),
