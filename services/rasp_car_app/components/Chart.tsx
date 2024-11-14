f0f966d8a1426a321f71470487f96e16af71f59e
etxsA <93368439+etxsA@users.noreply.github.com>
2024-11-14 00:59:01 -0600

    update: Finished Final Layout for Main page
    "

--- services/rasp_car_app/components/Chart.tsx
+++ services/rasp_car_app/components/Chart.tsx
@@ -18,7 +18,7 @@ type ChartProps = {
 };
 
 const Chart: React.FC<ChartProps> = ({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
+  <ResponsiveContainer width="100%" height="100%">
     <LineChart data={data}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
